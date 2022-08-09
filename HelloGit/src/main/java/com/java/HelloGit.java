310d66e9f03df2a4cabfebb0e9b83da6baa29287
70757 <707573956@qq.com>
2022-08-09 14:52:33 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/java/HelloGit.java
@@ -0,0 +1,9 @@
+package com.java;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
