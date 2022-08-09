71ae15292e722b9039a7331e97262aea64dd6461
70757 <707573956@qq.com>
2022-08-09 16:13:30 +0800

    克隆者提交

--- HelloGit/src/main/java/com/java/HelloGit.java
+++ HelloGit/src/main/java/com/java/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("主干添加");
         System.out.println("分支添加");
         System.out.println("在线添加");
+        System.out.println("clone project");
     }
 }
